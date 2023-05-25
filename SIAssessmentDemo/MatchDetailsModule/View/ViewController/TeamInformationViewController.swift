02b0379f1c08144b104b26bfd1166629273fc76e
platini93 <114639278+platini93@users.noreply.github.com>
2023-05-25 11:22:24 +0530

    cleanup

--- SIAssessmentDemo/MatchDetailsModule/View/ViewController/TeamInformationViewController.swift
+++ SIAssessmentDemo/MatchDetailsModule/View/ViewController/TeamInformationViewController.swift
@@ -13,15 +13,6 @@ class TeamInformationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Team 1 player 1 = \(matchData.teamsData.four.players.player1.name_full)")
-        print("Team 1 player 2 = \(matchData.teamsData.four.players.player2.name_full)")
-        print("Team 1 player 3 = \(matchData.teamsData.four.players.player3.name_full)")
-        print("Team 1 player 4 = \(matchData.teamsData.four.players.player4.name_full)")
-        print("Team 1 player 5 = \(matchData.teamsData.four.players.player5.name_full)")
-        print("\n\n")
-        print("Team 2 player 1 = \(matchData.teamsData.five.players.player1.name_full)")
-        print("Team 2 player 2 = \(matchData.teamsData.five.players.player2.name_full)")
-        
         indiaTeam = [matchData.teamsData.four.players.player1, matchData.teamsData.four.players.player2, matchData.teamsData.four.players.player3, matchData.teamsData.four.players.player4, matchData.teamsData.four.players.player5, matchData.teamsData.four.players.player6, matchData.teamsData.four.players.player7,
                      matchData.teamsData.four.players.player8, matchData.teamsData.four.players.player9, matchData.teamsData.four.players.player10, matchData.teamsData.four.players.player11]
         
